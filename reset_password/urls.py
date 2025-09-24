aa1d5cf7d63092000295a28dd634b0ec05dbffea
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2025-09-24 13:40:11 +0600

    removed the unused view

--- reset_password/urls.py
+++ reset_password/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('reset_password/', views.reset_password, name='reset_password'),
-    path('webhook/password-reset/', views.password_reset_webhook, name='password_reset_webhook'),
 ]
